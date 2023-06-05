38dbe58c755cc0b4959a71c6eee335c9fb640d81
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2023-06-05 23:08:32 +0800

    Delete MFComp Endterm/ajax_files directory

--- MFComp Endterm/ajax_files/deleteUser.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$id = $_POST['akoSiID'];
-	
-	$conn = mysqli_connect("localhost","root","","db_mfcomp");
-				
-	$query = $conn->query("DELETE FROM tbl_users WHERE user_id = '$id'");
-	
-?>
