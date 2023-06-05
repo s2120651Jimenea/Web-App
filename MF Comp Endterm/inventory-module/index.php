50f948c7841e163392ed1080a9faf4d76bfa4f7f
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2023-06-05 23:24:53 +0800

    Add files via upload

---
+++ MF Comp Endterm/inventory-module/index.php
@@ -0,0 +1,11 @@
+    <div id="subcontent">
+    <?php
+      switch($action){
+                case 'result':
+                    require_once 'inventory-module/search-user.php';
+                break;
+                default:
+                    require_once 'inventory-module/main.php';
+                break; 
+            }
+    ?>
